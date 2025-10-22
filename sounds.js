fc558d256f7100023fa49ff2f7a2d3af1d5e7be0
Skeo <skeo051600@gmail.com>
2025-10-21 21:54:23 -0500

    Start layout

---
+++ sounds.js
@@ -0,0 +1,15 @@
+const sounds = [
+  {
+    id: 1,
+    name: "boom",
+    location: "./sounds/boom.wav",
+    buttonAssigned: "shiftKey",
+    active: true
+  }
+]
+
+
+// id, name, sound location, button assigned,
+// if deleted, adjust property active
+// && active - main display
+// else, other array
